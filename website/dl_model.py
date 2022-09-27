325f1b98fd1a4cb94ed4e93d9408848302630a09
ahmohamed1 <abdll1@hotmail.com>
2022-09-27 12:33:09 +0300

    create class to get the paths of training files. Create class to create the model based on the selecttion with removing the top and inserting the output size.

--- website/dl_model.py
+++ website/dl_model.py
@@ -1,23 +1,39 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.resnet import ResNet101
+from tensorflow.keras.applications.resnet import ResNet152
+from tensorflow.keras.applications.resnet import ResNet50
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg19 import VGG19
 
-class Model():
+class Create_model():
     def __init__(self, model, optimizer, learning_rate, loss_function, batch_size):
         self.batch_size = batch_size
         self.optimizer = optimizer
         self.learning_rate = learning_rate
         self.loss_function = loss_function
+        self.model_selected = model
 
-        if model == 'ResNet10':
-            from tf.keras.applications.resnet10 import ResNet10 as model
-        elif model == 'ResNet18':
-            from tf.keras.applications.resnet18 import ResNet18 as model
-        elif model == 'ResNet50':
-            from tf.keras.applications.resnet50 import ResNet50 as model
-        elif model == 'VGG16':
-            from tf.keras.applications.vgg16 import VGG16 as model
-        elif model == 'VGG19':
-            from tf.keras.applications.vgg19 import VGG19 as model
+    def build(self, output_size):
+        input_shape = [255,255,3]
+        if self.model_selected == 'ResNet101':
+            model = ResNet101(input_shape=input_shape, weights='imagenet', include_top=False)
+        elif self.model_selected == 'ResNet152':
+            model = ResNet152(input_shape=input_shape, weights='imagenet', include_top=False)
+        elif self.model_selected == 'ResNet50':
+            model = ResNet50(input_shape=input_shape, weights='imagenet', include_top=False)
+        elif self.model_selected == 'VGG16':
+            model = VGG16(input_shape=input_shape, weights='imagenet', include_top=False)
+        elif self.model_selected == 'VGG19':
+            model = VGG19(input_shape=input_shape, weights='imagenet', include_top=False)
+        else:
+            print("[ERROR] No model selected")
 
-    def build(self):
-        pass
+        x = Flatten()(model.output)
+        output_layer = Dense(output_size, activation='softmax')(x)
+        model = Model(inputs=model.input, outputs=output_layer)
+        model.summary()
+
+
 
--- website/views_training.py
+++ website/views_training.py
@@ -7,6 +7,7 @@ import os
 import shutil
 from werkzeug.utils import secure_filename
 from .database_model import Project, Classes, db
+from .dl_model import Create_model
 from wtforms_sqlalchemy.fields import QuerySelectField
 
 
@@ -15,9 +16,25 @@ from wtforms_sqlalchemy.fields import QuerySelectField
 
 views_training = Blueprint('views_training', __name__)
 
+class ProjectDetails():
+    def __init__(self):
+        project_name = session['project']
+        project = Project.query.filter_by(name=project_name).first()
+        clases_s = Classes.query.filter_by(project_id=project.id).all()
+        self.project_id = project.id
+        self.classes_path  = []
+        for cls in clases_s:
+            path_ = os.path.join('projects', project_name, 'dataset', 'train', cls.className)
+            self.classes_path.append(path_)
 
+    def get_paths(self):
+        for path in self.classes_path:
+            print(path)
+
+    def get_class_number(self):
+        return len(self.classes_path)
 class ModelForm(FlaskForm):
-    select_model = SelectField('select_model', choices=['ResNet10','ResNet18','ResNet50',
+    select_model = SelectField('select_model', choices=['ResNet50','ResNet101','ResNet152',
                                                         'VGG16','VGG19'])
     batch_size = SelectField('batch_size', choices=['8','16','32'])
     learning_rate = FloatField('learning_rate', validators=[validators.InputRequired()])
@@ -28,18 +45,23 @@ class ModelForm(FlaskForm):
                                                           'categorical_crossentropy',
                                                           'sparse_categorical_crossentropy',
                                                           'kullback_leibler_divergence'])
+    submit_setup = SubmitField('Setup the model')
     submit_start = SubmitField('Start training')
     submit_stop = SubmitField('Stop training')
 
 
 @views_training.route('/train_page', methods=['GET', 'POST'])
 def train_page():
+    project_details = ProjectDetails()
     form = ModelForm(meta={'csrf': False})
-
     if form.validate_on_submit():
-        print(form.loss_function.data)
-        print(form.optimizer.data)
-        print(form.learning_rate.data)
+        if form.submit_setup.data:
+            created_mode = Create_model(form.select_model.data,
+                                        form.optimizer.data,
+                                        form.learning_rate.data,
+                                        form.loss_function.data,
+                                        form.batch_size.data)
+            created_mode.build(project_details.get_class_number())
         if form.submit_start.data:
             print("Start")
         if form.submit_stop.data:
